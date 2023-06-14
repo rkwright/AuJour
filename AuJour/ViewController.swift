bb1fcb8a53792ba0f836cd12ec015e45030842bd
Ric Wright <rkwright@geofx.com>
2023-06-14 12:02:57 -0700

    Initial commit

---
+++ AuJour/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuJour
+//
+//  Created by Richard Wright on 6/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
