01de2a71f0f1b3567f654df4a6e2c0d50f0637bd
Ric Wright <rkwright@geofx.com>
2023-06-15 11:23:09 -0700

    Completed section 4 of the tutorial

---
+++ AuJour/ReminderListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AuJour
+//
+//  Created by Richard Wright on 6/14/23.
+//
+
+import UIKit
+
+class ReminderListViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let listLayout = listLayout()
+        collectionView.collectionViewLayout = listLayout
+    }
+
+    private func listLayout() -> UICollectionViewCompositionalLayout {
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .grouped)
+        listConfiguration.showsSeparators = false
+        listConfiguration.backgroundColor = .clear
+        
+        return UICollectionViewCompositionalLayout.list(using: listConfiguration)
+    }
+    
+}
+
